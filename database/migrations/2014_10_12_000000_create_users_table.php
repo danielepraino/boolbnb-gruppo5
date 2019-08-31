6f35371eca7a572612f49cb601b49560fd182236
Daniele Praino <daniele.praino@gmail.com>
2019-08-31 10:12:12 +0200

    aggiunti nullable per la tabella users, aggiunto il faker zaninotto per creare i seed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('birthdate');
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('birthdate')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
