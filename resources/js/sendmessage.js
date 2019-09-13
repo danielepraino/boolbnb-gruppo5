6221356cca2133988020952782a15c3dd999f9a8
naplazz <naplazz@gmail.com>
2019-09-13 17:23:56 +0200

    modal fixed, modal added in apt

--- resources/js/sendmessage.js
+++ resources/js/sendmessage.js
@@ -1,28 +1,28 @@
-$('#send-btn').click(function() {
-  $.ajax({
-    headers: {
-
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-
-        },
-  url:"http://127.0.0.1:8000/sendmessage",
-  type: "POST",
-  data: {
-    flat_id: $('#flat_id').val(),
-    sender: $('#sender').val(),
-    subject: $('#subject').val(),
-    message: $('#message').val(),
-
-  },
-  success:function(data){
-
-  alert(data.success);
-
-  },
-  error: function(){
-
-  alert('errore');
-  }
-  });
-
-});
+// $('#send-btn').click(function() {
+//   $.ajax({
+//     headers: {
+//
+//             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+//
+//         },
+//   url:"http://127.0.0.1:8000/sendmessage",
+//   type: "POST",
+//   data: {
+//     flat_id: $('#flat_id').val(),
+//     sender: $('#sender').val(),
+//     subject: $('#subject').val(),
+//     message: $('#message').val(),
+//
+//   },
+//   success:function(data){
+//
+//   alert(data.success);
+//
+//   },
+//   error: function(){
+//
+//   alert('errore');
+//   }
+//   });
+//
+// });
