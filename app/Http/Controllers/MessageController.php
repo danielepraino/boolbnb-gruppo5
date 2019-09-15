93b7bd9e4176abf7e1c7adfbea92ca107189cc90
andrea-scm <a.scimitto@gmail.com>
2019-09-15 17:53:28 +0200

    aggiunti controlli sui form dei messaggi,cambiata estensione per register/login di laravel

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -38,11 +38,18 @@ class MessageController extends Controller
      */
     public function store(Request $request)
     {
+      //validazione dati
+      $validatedData = $request->validate([
+        "sender" => "required|max:255|regex:/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/",
+        "subject" => "required|max:50|regex:/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/",
+        "message" => "required|regex:/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/|max:255"
+      ]);
+
       $data = $request->all();
       $newMessage = new Message();
       $newMessage->fill($data);
       $newMessage->save();
-      return redirect('/messages');
+      return redirect('/');
 
     }
 
